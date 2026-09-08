75a7d4253dccef48e05a70eb2faff8d2369f972f
Conrad008 <kipngeno7180@gmail.com>
2026-09-08 21:17:09 +0300

    feat: setup base user dependencies and validators

--- CNR_app/models.py
+++ CNR_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import MinValueValidator
+from django.utils import timezone
