cc84f37a535eac6dd5a7a3af76edeb4099356673
Conrad008 <kipngeno7180@gmail.com>
2026-09-08 21:21:22 +0300

    feat: implement custom user manager with user and superuser creation methods for email-based authentication

--- CNR_app/models.py
+++ CNR_app/models.py
@@ -3,3 +3,20 @@ import uuid
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MinValueValidator
 from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('username', email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', User.Role.SUPER_ADMIN)
+        return self.create_user(email, password, **extra_fields)
