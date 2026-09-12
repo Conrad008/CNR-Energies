975ebae3723cc8c37351a6498b3d6269cfdfc8f0
Conrad008 <kipngeno7180@gmail.com>
2026-09-12 13:42:31 +0300

    feat:Add HasRole permission class for role-based access control

--- CNR_app/permissions.py
+++ CNR_app/permissions.py
@@ -1,3 +1,13 @@
 from rest_framework.permissions import BasePermission
 from CNR_app.models import User
 
+class HasRole(BasePermission):
+    allowed_roles = ()
+
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(
+            user and
+            user.is_authenticated and
+            user.role in self.allowed_roles
+        )
