8a558217f6ef3c6d61b7c0702ad0a3e717168de8
Conrad008 <kipngeno7180@gmail.com>
2026-09-12 14:10:23 +0300

    feat: Add core authentication and user management API views and permissions

--- CNR_app/views.py
+++ CNR_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairSerializer, TokenObtainPairView
+from CNR_app.models import User
+from CNR_app.serializers import CustomTokenObtainPairSerializer, UserSerializer, UserCreateSerializer
+from CNR_app.permissions import IsSuperAdmin, IsManagerOrAdmin
