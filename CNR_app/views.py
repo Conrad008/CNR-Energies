4a13b874e9aff2eda697660984fd8a9a8123e516
Conrad008 <kipngeno7180@gmail.com>
2026-09-12 14:20:47 +0300

    feat: Implement user profile retrieval and update functionality

--- CNR_app/views.py
+++ CNR_app/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairSerializer, TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView
 from CNR_app.models import User
 from CNR_app.serializers import CustomTokenObtainPairSerializer, UserSerializer, UserCreateSerializer
 from CNR_app.permissions import IsSuperAdmin, IsManagerOrAdmin
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class CurrentUserProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
