8b86c4c1f98dbb15fba4e8b57bfa4de8f81bc003
Conrad008 <kipngeno7180@gmail.com>
2026-09-12 23:12:44 +0300

    feat: Update URL patterns for user management and authentication views

--- CNR_app/urls.py
+++ CNR_app/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from CNR_app.views import (CustomTokenObtainPairView, CurrentUserProfileView, UserListCreateView, UserDetailView,)
+
+urlpatterns = [
+    path('auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/me/', CurrentUserProfileView.as_view(), name='user_profile'),
+    
+    path('users/', UserListCreateView.as_view(), name='user_list_create'),
+    path('users/<uuid:pk>/', UserDetailView.as_view(), name='user_detail'),
+]
