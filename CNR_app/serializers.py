1e55f30fb9a0c13e1772371237e137f9b71a57c7
Conrad008 <kipngeno7180@gmail.com>
2026-09-12 13:11:51 +0300

    feat: implement custom JWT serializer to include user details in token and response

--- CNR_app/serializers.py
+++ CNR_app/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from CNR_app.models import User
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Include custom claims in the JWT payload
+        token['email'] = user.email
+        token['role'] = user.role
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        # Include user details in the login response body
+        data['user'] = {
+            'id': str(self.user.id),
+            'email': self.user.email,
+            'role': self.user.role,
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+        }
+        return data
