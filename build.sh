2ec7e40be5464eb40698be1c57bd9009954eb817
Conrad008 <kipngeno7180@gmail.com>
2026-09-21 18:21:44 +0300

    feat: Add initial seeding and superuser creation to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py seed_station_data
+
+python manage.py shell -c "
+from core.models import User;
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('admin@cnrenergies.com', 'Ngeno208', role='SUPER_ADMIN')
+    print('Superuser created successfully.')
+"
