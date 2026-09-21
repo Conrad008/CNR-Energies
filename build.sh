e0222fd17eeae2a74993bc612f45e7a1ea2f7477
Conrad008 <kipngeno7180@gmail.com>
2026-09-21 18:25:55 +0300

    fix: Correct seedData command in build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py seed_station_data
+python manage.py seedData
 
 python manage.py shell -c "
 from core.models import User;
