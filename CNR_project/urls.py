1eb4c14645e7238ba8851871b1a6a01a5ed91ca5
Conrad008 <kipngeno7180@gmail.com>
2026-09-21 18:05:15 +0300

    refactor: Remove home view and its URL pattern from urlpatterns

--- CNR_project/urls.py
+++ CNR_project/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
 
-def home(request):
-    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('CNR_app.urls')),
-    path('', home, name='home'),
 ]
